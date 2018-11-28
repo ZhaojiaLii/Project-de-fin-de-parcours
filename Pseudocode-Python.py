8854f4349bead4a27740f0d087478b641d09fa7c
Li Zhaojia <alpacaaa3@gmail.com>
2018-11-28 19:18:37 +0100

    增加list的初始化及赋值功能 可print已经初始化并赋值的list 增加for循环功能，暂未完成 更新dot结构图

--- Pseudocode-Python.py
+++ Pseudocode-Python.py
@@ -22,10 +22,15 @@ class Python(object):
         # we put the variables we have declared in the list, is easier to use and process
         declare_list = []
         input_list = []
+        list_name = []  # save the list name 
         need_value_list = []
-        have_value_list = []
+        have_value_list = []  
+        list_already_have_value = []
+        list_list = []  # insure the variable is a list and already in our list_name
         if_num_1 = 0    # let us know the number of the if instruction
+        for_num_1 = 0   # let us know the number of the for instruction
         indentation = "    "    # control the indentation of python code
+        indentation_for="   "
 
         
         # where we get the instructions from the model and process them
@@ -43,17 +48,56 @@ class Python(object):
             elif if_num_1 == 2:
                 indentation = "        "
             elif if_num_1 > 2:
+                # the "i" here is not used but necessary to run the loop
                 for i in range(if_num_1-2):
                     indentation = indentation + "    "
+            
+            if for_num_1 == 0:
+                indentation_for=""
+            elif for_num_1 == 1:
+                indentation_for="    "
+            elif for_num_1 == 2:
+                indentation_for = "        "
+            elif for_num_1 > 2:
+                for i in range(for_num_1-2):
+                    indentation_for = indentation_for + "    "
 
             # we start to process the pseudocode from here:
             if c.__class__.__name__ == 'Declare_variable':                
                 var_declare = '{}'.format(c.variable.var)
                 declare_list.append(var_declare)
                 need_value_list.append(var_declare)
+
                 # here we can show the user which variable they have declared
                 print ("//you have declared variable " + var_declare)
                 
+            # we can declare list and print <variable = []>
+            elif c.__class__.__name__ == 'Declare_list':
+                list__name = '{}'.format(c.variable.var)
+                list_name.append(list__name)
+                print (list__name + " = []")
+                
+            
+            # detect if the name of the list has been declared or not
+            elif c.__class__.__name__ == 'Input_list':
+                verify_name = '{}'.format(c.variable.var)
+                add_to_list = []
+                print_list = []
+                for list_variables in c.list_variables:
+                    final_list_variable = list_variables.list_var
+                    add_to_list.append(final_list_variable.encode('raw_unicode_escape'))  
+
+                # assign values to the list
+                # if the list has already declared, we put the elements in the list and print
+                if verify_name in list_name:
+                    right_name = verify_name
+                    list_already_have_value.append(right_name)
+                    for list_elements in add_to_list:    
+                        print_list.append(list_elements)
+                    print (right_name + ' = %s' %print_list)
+                else:
+                    print ('// can not find the list name:"' + verify_name + '", please declare first')
+
             elif c.__class__.__name__ == 'Input_variable':
                 var_input = '{}'.format(c.variable.var)
                 # detect the list include the variable we want to input or not
@@ -66,8 +110,13 @@ class Python(object):
                     input_list.append(var_input)
                     if if_num_1 != 0:
                         print (indentation + var_input + " = raw_input('input:"+var_input+"')" )
-                    else:
-                        print (var_input + " = raw_input('input:"+var_input+"')" )  
+                    else: 
+                        if for_num_1!= 0:
+                            print (indentation_for + var_input + " = raw_input('input:"+var_input+"')" )
+                            if isinstance(var_declare.list):   # 判断是否是list?????
+                                list_list.append(var_declare)  
+                        else:
+                            print (var_input + " = raw_input('input:"+var_input+"')" )    
 
             elif c.__class__.__name__ == 'Declare_value':
                 # here we can assign the value to the variables we have declared
@@ -78,6 +127,8 @@ class Python(object):
                     value = str(c.value.val)
                     if if_num_1 != 0:
                         print (indentation + var_value+' = '+value)
+                    elif for_num_1!=0:
+                        print (indentation_for + var_value+' = '+value)
                     else:
                         print (var_value+' = '+value)
                     # we use the list to know which variable we have declared but not assigned value
@@ -87,6 +138,8 @@ class Python(object):
                     value = c.value.val
                     if if_num_1 != 0:
                         print (indentation + var_value + " = '"+value+"'")
+                    elif for_num_1!= 0:
+                        print (indentation_for + var_value + " = '"+value+"'")
                     else:
                         print (var_value+" = '"+value+"'")
                     need_value_list.remove(var_value)   
@@ -101,13 +154,37 @@ class Python(object):
                 if content in declare_list and input_list:
                     if if_num_1 != 0:
                         print (indentation + 'print '+ content)
+                    elif for_num_1 != 0:
+                        print (indentation_for + 'print '+ content)
                     else:
                         print ('print '+ content)
+                elif content in list_name:
+                    if content in list_already_have_value:
+                        if if_num_1 != 0:
+                            print (indentation + 'print '+ content)
+                        elif for_num_1 != 0:
+                            print (indentation_for + 'print '+ content)
+                        else:
+                            print ('print '+ content)
+                    else:
+                        if if_num_1 != 0:
+                            # remind the user if the list is empty or not existed
+                            print (indentation + 'print '+ content)
+                            print (indentation + '//can not print empty list, please assign values first')
+                        elif for_num_1 != 0:
+                            print (indentation_for + 'print '+ content)
+                            print (indentation_for + '//can not print empty list, please assign values first')
+                        else:
+                            print ('print '+ content)
+                            print ('//can not print empty list, please assign values first') 
+
                 elif content in declare_list not in input_list:
                     print ('\n'+'//'+content+' is a variable, please give a value before print.')
                 else:
                     if if_num_1 != 0:
                         print (indentation + 'print "'+ content +'"')
+                    elif for_num_1!= 0:
+                        print (indentation_for + 'print "'+ content+'"')
                     else:
                         print ('print "'+ content +'"')
 
@@ -115,9 +192,11 @@ class Python(object):
                 content = '{}'.format(c.content_string.con)
                 if if_num_1 != 0:
                     print (indentation + 'print '+'"'+content+'"')
+                elif for_num_1 != 0:
+                    print (indentation_for + 'print '+'"'+content+'"')
                 else:
                     print ('print '+'"'+content+'"')
-            
+
             elif c.__class__.__name__ == 'Calculation_simple':
                 # we can print the simple calculations here
                 variable_af = '{}'.format(c.variable_af.var_af)
@@ -239,7 +318,6 @@ class Python(object):
                 # we use the symbol "a" to know if the two variables are both declared
                 # if a=2, we go into the if instruction and print the python code
                 if a == 2:
-
                     # comparasion in if instruction
                     if comparasion_symbol == 'is greater than':
                         print (indentation + "elif " + variable_be + " > " + variable_af + ":")                      
@@ -280,9 +358,56 @@ class Python(object):
             elif c == 'end if':
                 print ("//number " + str(if_num_1) + " if instruction finished")
                 if_num_1 = if_num_1 - 1
+
+            # for conditions is below:   
+            #first one is related to list:
+            elif c.__class__.__name__ == 'For_instruction_startline_1':
+                indentation_for = "    "
+                a=0
+                variable_main = '{}'.format(c.variable_main.var_main)
+                variable_list = '{}'.format(c.variable_range.var_list)
+
+            # ensure the parameters is avaliable
+                if isinstance(variable_main,str):
+                    a = a + 1                
+                else:
+                    print ("//" + variable_main + " is not a string, please use string")
                 
+                if variable_list in list_list:
+                    a = a + 1                
+                elif variable_list in declare_list:
+                    print ("//" + variable_list + " is not a valued variable, please give a value first.")  
+                else:
+                    print ("//" + variable_list + " is not a declared variable, please declare first.")
+                
+                if a ==2:
+                    print ("for "+variable_main+" in "+variable_range+" do")
 
+            #second one is related to range:
+            elif c.__class__.__name__ == 'For_instruction_startline_2':
+                indentation_for = "    "
+                a=0
+                variable_main = '{}'.format(c.variable_main.var_main)
+                variable_range = '{}'.format(c.variable_range.var_range)
 
+                # ensure the parameters is avaliable
+                if isinstance(variable_main,str):
+                    a = a + 1                
+                else:
+                    print ("//" + variable_main + " is not a string, please use string")
+                
+                if isinstance(variable_range,int):
+                    a = a + 1                
+                else:
+                    print ("//" + variable_list + " is not a number, please give a number first.")  
+                
+                if a ==2:
+                    print ("for "+variable_main+" in range("+variable_range+") do")
+                
+                
+            elif c == 'end for':
+                print ("//for instruction finished")
+                for_num_1 = for_num_1 - 1
                 
         # show the values we have not given values at the last of the python code
 
